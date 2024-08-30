144d7372d40d32c33864ed22b95a12b1d3b3aa89
mhishmeh <hishmoe@gmail.com>
2024-08-30 15:40:25 -0500

    added containers to sprite groups

--- main.py
+++ main.py
@@ -7,15 +7,25 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     fps = pygame.time.Clock()
     dt = 0
+    
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable,drawable)
+
     Player_one = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT / 2,PLAYER_RADIUS)
+
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
-        Player_one.draw(screen)
-        Player_one.update(dt)
+        for player in updateable:
+            player.update(dt)
+        for player in drawable:
+            player.draw(screen)
         pygame.display.flip()
 
         dt = fps.tick(60) / 1000
