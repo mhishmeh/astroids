e3167d76363bc6b92d8be749ba0b55e172e5bf28
mhishmeh <hishmoe@gmail.com>
2024-08-31 17:16:43 -0500

    fully completed asteroid game. entirely in pygame.

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 from circleshape import *
 from pygame import *
+from constants import *
+import random
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
@@ -11,3 +13,20 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            angle = random.uniform(20,50)
+            Vector1 = pygame.math.Vector2.rotate(self.velocity, angle)
+            Vector2 = pygame.math.Vector2.rotate(self.velocity, -(angle))
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            
+            asteroid_1 = Asteroid(self.position.x, self.position.y,new_radius)
+            asteroid_2 = Asteroid(self.position.x,self.position.y,new_radius)
+
+            asteroid_1.velocity = Vector1 * 1.2
+            asteroid_2.velocity = Vector2 * 1.2
+        
--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def main():
                 for shot in shots:
                     if shot.Check_Collision(asteroid):
                         shot.kill()
-                        asteroid.kill()
+                        asteroid.split()
 
                 
         screen.fill(color="black")
